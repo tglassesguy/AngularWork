0751d0cad61db21938c2e0d4758a44af1ed64ffb
cliffjav <jcollazosgomez@gmail.com>
2016-04-20 16:18:29 -0500

    iniciando clases

---
+++ Curso de Angular/Video 3 - Modelo en Scope/js/index.js
@@ -0,0 +1,7 @@
+function AlumnoController($scope) {
+    $scope.alumno = {
+        nombre: "Javier Collazos",
+        curso: "Ingenieria de sistemas",
+        universidad: "Universidad de Ibague"
+    }
+}
---
+++ Curso de Angular/Video 3 - ejercicio de prueba/js/index.js
@@ -0,0 +1,7 @@
+function DatosController($scope) {
+    $scope.Datos = {
+        nombre: "Javier Collazos",
+        edad: "17 años",
+        peso: "45 kg"
+    }
+}
