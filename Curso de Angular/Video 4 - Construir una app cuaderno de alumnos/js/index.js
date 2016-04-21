1da5b4bc43b607b0bf4fd80fe674cec33f42c9fa
cliffjav <jcollazosgomez@gmail.com>
2016-04-20 23:41:57 -0500

    day 1

--- Curso de Angular/Video 4 - Construir una app cuaderno de alumnos/js/index.js
+++ Curso de Angular/Video 4 - Construir una app cuaderno de alumnos/js/index.js
@@ -1,8 +1,14 @@
 function AlumnosController($scope) {
         $scope.alumnos=[
-            {nombre:"Javier Collazos", telefono: "95987894", curso:"clase1"},
-            {nombre:"Lola Rulos", telefono: "9598498844", curso:"clase2"},
-            {nombre:"sdfsdfasd Lola", telefono: "1981987894", curso:"clase3"},
-            {nombre:"Jasdfasdfs", telefono: "9598948984894", curso:"clase4"}
+            {nombre:"Javier Collazos", telefono: "195987894", curso:"clase1"},
+            {nombre:"Lola Rulos", telefono: "29598498844", curso:"clase2"},
+            {nombre:"sdfsdfasd Lola", telefono: "31981987894", curso:"clase3"},
+            {nombre:"Jasdfasdfs", telefono: "49598948984894", curso:"clase4"}
         ];
+$scope.Save=function(){
+
+    $scope.alumnos.push({nombre:$scope.nuevoAlumno.nombre,
+                         telefono: $scope.nuevoAlumno.telefono,
+                         curso: $scope.nuevoAlumno.curso})
+                     }
 }
